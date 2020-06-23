1b152b5d45ff1dc89d319605ce0fe4d4ce245dda
goonerholic <goonerholic@gmail.com>
2020-06-24 00:57:29 +0900

    add header component(on progress)

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,24 @@
 import React, { ReactElement } from 'react';
+import { Navbar, Icon, Button } from 'react-materialize';
+import './Header.scss';
 
 interface Props {}
 
 export default function Header({}: Props): ReactElement {
-	return <div></div>;
+	return (
+		<Navbar
+			className="Header"
+			alignLinks="right"
+			brand={
+				<a className="brand-logo" href="/">
+					GOONERHOLIC
+				</a>
+			}
+			menuIcon={<Icon>menu</Icon>}
+		>
+			<a className="btn cyan waves-effect waves-light" href="/login">
+				sign in
+			</a>
+		</Navbar>
+	);
 }

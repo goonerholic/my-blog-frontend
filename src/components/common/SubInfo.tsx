0c73c627f6aad9cded7f544585e779209874b6f3
goonerholic <goonerholic@gmail.com>
2020-06-26 18:01:38 +0900

    split PowerViewer component
    add postList component
    split subinfo and tags as seperate components

---
+++ src/components/common/SubInfo.tsx
@@ -0,0 +1,26 @@
+import React, { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
+import './SubInfo.scss';
+
+interface Props {
+	username: string;
+	publishedDate: Date;
+	hasMarginTop?: boolean;
+}
+
+export default function SubInfo({
+	username,
+	publishedDate,
+	hasMarginTop,
+}: Props): ReactElement {
+	return (
+		<div className={`SubInfo${hasMarginTop ? ' has-margin-top' : ''}`}>
+			<span>
+				<b>
+					<Link to={`/@${username}`}>{username}</Link>
+				</b>
+			</span>
+			<span>{publishedDate.toLocaleDateString()}</span>
+		</div>
+	);
+}
